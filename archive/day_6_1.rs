56b5cf038ee02f5d548cf19a4fa8781d8f016900
Satyarth Agrahari <satyarthagrahari@gmail.com>
2021-12-06 12:53:10 +0530

    day 6

--- archive/day_6_1.rs
+++ archive/day_6_1.rs
@@ -1,11 +1,5 @@
-use std::collections::HashMap;
-
 fn main() {
-    let mut ages = HashMap::new();
-    (0..=8).for_each(|i| {
-        ages.insert(i, 0);
-    });
-    vec![
+    let mut ages = vec![
         1, 1, 3, 5, 3, 1, 1, 4, 1, 1, 5, 2, 4, 3, 1, 1, 3, 1, 1, 5, 5, 1, 3, 2, 5, 4, 1, 1, 5, 1,
         4, 2, 1, 4, 2, 1, 4, 4, 1, 5, 1, 4, 4, 1, 1, 5, 1, 5, 1, 5, 1, 1, 1, 5, 1, 2, 5, 1, 1, 3,
         2, 2, 2, 1, 4, 1, 1, 2, 4, 1, 3, 1, 2, 1, 3, 5, 2, 3, 5, 1, 1, 4, 3, 3, 5, 1, 5, 3, 1, 2,
@@ -16,18 +10,20 @@ fn main() {
         1, 1, 3, 5, 1, 1, 4, 2, 1, 2, 5, 2, 5, 1, 1, 1, 2, 3, 5, 5, 1, 4, 3, 2, 2, 3, 2, 1, 1, 4,
         1, 3, 5, 2, 3, 1, 1, 5, 1, 3, 5, 1, 1, 5, 5, 3, 1, 3, 3, 1, 2, 3, 1, 5, 1, 3, 2, 1, 3, 1,
         1, 2, 3, 5, 3, 5, 5, 4, 3, 1, 5, 1, 1, 2, 3, 2, 2, 1, 1, 2, 1, 4, 1, 2, 3, 3, 3, 1, 3, 5,
-    ]
-    .into_iter()
-    .for_each(|e| {
-        *ages.get_mut(&e).unwrap() += 1;
-    });
+    ];
     for day in 0..256 {
-        let z = ages[&0];
-        for i in 1..=8 {
-            *ages.get_mut(&(i - 1)).unwrap() = ages[&i];
+        let mut new_ages = ages.clone();
+        let mut to_add = 0;
+        for x in &mut new_ages {
+            if *x == 0 {
+                *x = 6;
+                to_add += 1;
+            } else {
+                *x -= 1;
+            }
         }
-        *ages.get_mut(&6).unwrap() += z;
-        *ages.get_mut(&8).unwrap() = z;
-        println!("day: {}, fishes: {}", day, ages.values().sum::<i64>());
+        new_ages.append(&mut vec![8; to_add]);
+        ages = new_ages;
+        println!("day: {}, fishes: {}", day, ages.len());
     }
 }
