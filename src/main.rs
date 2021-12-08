8e7857dfcc39fa4b3c5ebf071a88f250adfc997f
Satyarth Agrahari <satyarthagrahari@gmail.com>
2021-12-08 13:19:04 +0530

    changing day 8 to be constant time signal

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,4 @@
-fn next_permutation<T>(arr: &mut [T]) -> bool
-where
-    T: std::cmp::Ord,
-{
-    use std::cmp::Ordering;
-
-    let last_ascending = match arr.windows(2).rposition(|w| w[0] < w[1]) {
-        Some(i) => i,
-        None => {
-            arr.reverse();
-            return false;
-        }
-    };
-    let swap_with = arr[last_ascending + 1..]
-        .binary_search_by(|n| match arr[last_ascending].cmp(n) {
-            Ordering::Equal => Ordering::Greater,
-            ord => ord,
-        })
-        .unwrap_err();
-    arr.swap(last_ascending, last_ascending + swap_with);
-    arr[last_ascending + 1..].reverse();
-    true
-}
+use std::collections::HashMap;
 
 fn resolve(base: &Vec<char>, n: &str) -> i64 {
     let base: String = base.iter().collect();
@@ -64,38 +42,67 @@ fn valid(base: &Vec<char>, nos: &Vec<&str>) -> bool {
     got.into_iter().all(|e| e)
 }
 
+fn missing(req: &Vec<char>, has: &Vec<char>) -> Vec<char> {
+    let mut m = vec![];
+    for c in req {
+        if !has.contains(c) {
+            m.push(*c);
+        }
+    }
+    m
+}
+
+fn generate_signal(nos: &mut Vec<&str>) -> Vec<char> {
+    let mut signal = vec!['a'; 7];
+
+    let mut freq: HashMap<char, usize> = "abcdefg".chars().map(|c| (c, 0)).collect();
+    nos.into_iter().for_each(|n| {
+        n.chars().for_each(|c| {
+            *freq.get_mut(&c).unwrap() += 1;
+        });
+    });
+    freq.iter().for_each(|(c, f)| match *f {
+        9 => signal[4] = *c,
+        6 => signal[3] = *c,
+        4 => signal[6] = *c,
+        _ => {}
+    });
+    nos.sort_by(|a, b| a.len().cmp(&b.len()));
+    let one: Vec<char> = nos[0].chars().collect();
+    signal[1] = missing(&one, &vec![signal[4]])[0];
+    let seven: Vec<char> = nos[1].chars().collect();
+    signal[0] = missing(&seven, &vec![signal[1], signal[4]])[0];
+    let four: Vec<char> = nos[2].chars().collect();
+    signal[2] = missing(&four, &vec![signal[1], signal[3], signal[4]])[0];
+    signal[5] = signal[0];
+    signal[5] = missing(&(nos[9].chars().collect()), &(signal.clone()))[0];
+    signal
+}
+
 fn main() {
     let data = std::fs::read_to_string("input.txt").unwrap();
     let lines: Vec<&str> = data.split('\n').collect();
-    let mut signals = vec!['d', 'a', 'f', 'e', 'b', 'c', 'g'];
     let s = lines
         .into_iter()
         .filter(|line| line.len() > 0)
         .map(|line| {
             let mut splitter = line.split('|');
-            let nos = splitter
+            let mut nos = splitter
                 .next()
                 .unwrap()
                 .split(' ')
                 .filter(|c| c.len() > 0)
                 .collect::<Vec<_>>();
-            let query = splitter
+            let signal = generate_signal(&mut nos);
+            assert!(valid(&signal, &nos));
+            let mut num = 0;
+            splitter
                 .next()
                 .unwrap()
                 .split(' ')
                 .filter(|n| n.len() > 0)
-                .collect::<Vec<_>>();
-            loop {
-                if valid(&signals, &nos) {
-                    let mut num = 0;
-                    query
-                        .into_iter()
-                        .for_each(|n| num = num * 10 + resolve(&signals, n));
-                    dbg!(num);
-                    break num;
-                }
-                next_permutation(&mut signals);
-            }
+                .for_each(|n| num = num * 10 + resolve(&signal, n));
+            num
         })
         .sum::<i64>();
     println!("{}", s);
