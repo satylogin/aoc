5479d49fca26e290acf3603b3bc375130e7948fb
Satyarth Agrahari <satyarthagrahari@gmail.com>
2021-12-09 12:52:02 +0530

    adding time

--- archive-2021/day_9.rs
+++ archive-2021/day_9.rs
@@ -1,8 +1,9 @@
 use std::collections::VecDeque;
 
 fn main() {
+    let now = std::time::Instant::now();
     let data = std::fs::read_to_string("input.txt").unwrap();
-    let grid = data
+    let mut grid = data
         .split('\n')
         .filter(|l| !l.is_empty())
         .map(|l| l.chars().map(|c| c as u8 - '0' as u8).collect::<Vec<_>>())
@@ -10,14 +11,13 @@ fn main() {
 
     let n = grid.len();
     let m = grid[0].len();
-    let mut mark = vec![vec![false; m]; n];
     let mut lens = vec![];
     for i in 0..n {
         for j in 0..m {
-            if !mark[i][j] && grid[i][j] != 9 {
+            if grid[i][j] != 9 {
                 let mut q = VecDeque::new();
                 q.push_back((i as i32, j as i32));
-                mark[i][j] = true;
+                grid[i][j] = 9;
                 let mut len = 1;
                 while let Some((i, j)) = q.pop_front() {
                     for (x, y) in vec![(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)] {
@@ -25,10 +25,9 @@ fn main() {
                             && x < n as i32
                             && y >= 0
                             && y < m as i32
-                            && !mark[x as usize][y as usize]
                             && grid[x as usize][y as usize] != 9
                         {
-                            mark[x as usize][y as usize] = true;
+                            grid[x as usize][y as usize] = 9;
                             len += 1;
                             q.push_back((x, y));
                         }
@@ -40,4 +39,5 @@ fn main() {
     }
     lens.sort_by(|x, y| y.cmp(&x));
     println!("{}", lens[0] * lens[1] * lens[2]);
+    println!("elapsed: {}", now.elapsed().as_millis());
 }
