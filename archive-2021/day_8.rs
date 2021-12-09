13ffffe5bdccbe07c97feb997865be9a8d12fc82
Satyarth Agrahari <satyarthagrahari@gmail.com>
2021-12-09 12:41:54 +0530

    day 9

--- archive-2021/day_8.rs
+++ archive-2021/day_8.rs
@@ -1,37 +1,28 @@
 use std::collections::HashMap;
 
 fn signal_to_digit(rewired: Vec<char>) -> HashMap<String, usize> {
+    let c_rewired = |c: char| rewired[(c as u8 - 'a' as u8) as usize];
     vec![
         "abefgd", "be", "abcgf", "abcef", "dcbe", "adcef", "adgfec", "abe", "abcdefg", "abcdef",
     ]
     .into_iter()
-    .map(|s| {
-        let s = s
-            .chars()
-            .map(|c| rewired[(c as u8 - 'a' as u8) as usize])
-            .collect::<String>();
-        sort(&s)
-    })
-    .enumerate()
-    .map(|(n, s)| (s.to_string(), n))
+    .map(|s| sort(&s.chars().map(c_rewired).collect::<String>()))
+    .zip(0..10)
     .collect()
 }
 
 fn sort(s: &str) -> String {
     let mut s = s.chars().collect::<Vec<_>>();
-    s.sort();
+    s.sort_unstable();
     s.into_iter().collect()
 }
 
-fn missing(req: &Vec<char>, has: &Vec<char>) -> Vec<char> {
-    req.iter()
-        .filter(|c| !has.contains(c))
-        .map(|c| *c)
-        .collect()
+fn missing(req: Vec<char>, has: Vec<char>) -> Vec<char> {
+    req.into_iter().filter(|c| !has.contains(&c)).collect()
 }
 
 fn generate_wiring(signals: &mut Vec<&str>) -> Vec<char> {
-    let mut wiring = vec!['0'; 7];
+    let mut w = vec!['0'; 7];
 
     let mut freq: HashMap<char, usize> = "abcdefg".chars().map(|c| (c, 0)).collect();
     signals.into_iter().for_each(|n| {
@@ -40,25 +31,23 @@ fn generate_wiring(signals: &mut Vec<&str>) -> Vec<char> {
         });
     });
     freq.iter().for_each(|(c, f)| match *f {
-        9 => wiring[4] = *c,
-        6 => wiring[3] = *c,
-        4 => wiring[6] = *c,
+        9 => w[4] = *c,
+        6 => w[3] = *c,
+        4 => w[6] = *c,
         _ => {}
     });
 
-    signals.sort_by(|a, b| a.len().cmp(&b.len()));
-    wiring[1] = missing(&(signals[0].chars().collect()), &vec![wiring[4]])[0];
-    wiring[0] = missing(&(signals[1].chars().collect()), &vec![wiring[1], wiring[4]])[0];
-    wiring[2] = missing(
-        &(signals[2].chars().collect()),
-        &vec![wiring[1], wiring[3], wiring[4]],
-    )[0];
-    wiring[5] = missing(&(signals[9].chars().collect()), &(wiring.clone()))[0];
+    signals.sort_unstable_by(|a, b| a.len().cmp(&b.len()));
+    w[1] = missing(signals[0].chars().collect(), vec![w[4]])[0];
+    w[0] = missing(signals[1].chars().collect(), vec![w[1], w[4]])[0];
+    w[2] = missing(signals[2].chars().collect(), vec![w[1], w[3], w[4]])[0];
+    w[5] = missing(signals[9].chars().collect(), w.clone())[0];
 
-    wiring
+    w
 }
 
 fn main() {
+    let now = std::time::Instant::now();
     let data = std::fs::read_to_string("input.txt").unwrap();
     let lines = data.split('\n').filter(|l| l.len() > 0);
     let resolved = lines.map(|line| {
@@ -68,5 +57,6 @@ fn main() {
         let std = signal_to_digit(generate_wiring(&mut numbers.collect::<Vec<_>>()));
         output.fold(0, |num, n| num * 10 + std[&sort(n)])
     });
-    println!("{}", resolved.sum::<usize>());
+    println!("ans: {}", resolved.sum::<usize>());
+    println!("elapsed: {}", now.elapsed().as_millis());
 }
